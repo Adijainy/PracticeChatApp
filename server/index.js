7616b619ce41b64a1a89a59eb6ccb0b3ac56f7b4
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2024-01-21 16:00:23 +0530

    fixed issues and built the real time chat functionality

--- server/index.js
+++ server/index.js
@@ -1,22 +1,43 @@
-const io = require("socket.io")(5000, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
-});
-const express = require("express");
+const express = require('express');
+const {Server} = require('socket.io');
 
 const app = express();
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+
+const server = require('http').createServer(app);
+
+
+const io = new Server(server, {
+  cors:{
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
 });
 
-app.get("/", (req, res) => {
-  res.send("Server is running.");
+server.listen(4000, () => {
+    console.log('Server started on :4000');
 });
 
-io.on("connection", (socket) => {
-  console.log(socket.id);
-  socket.on("msg", (msg) => {
-    console.log(msg);
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected,',socket.id);
+  socket.on('msg', ({newMsg, roomName}) => {
+    if(!roomName){
+      socket.broadcast.emit('receive-msg', newMsg);
+    }else{
+      socket.to(roomName).emit('receive-msg', newMsg);
+    }
+    
   });
-});
+
+  socket.on('join-room', (roomName) => {
+    socket.join(roomName);
+    console.log(`Joined room ${roomName}`);
+  });
+
+
+})
+
+
